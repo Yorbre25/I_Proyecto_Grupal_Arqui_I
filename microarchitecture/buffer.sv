aa748571020a431f1687014ad0d7f0bd25c2b0e5
Luis Andrey Zuniga <luisandreyzuniga@gmail.com>
2023-04-28 20:30:17 -0600

    Updating buffer to async reset

--- microarchitecture/buffer.sv
+++ microarchitecture/buffer.sv
@@ -1,7 +1,7 @@
 module buffer #(parameter Buffer_size=32)(input rst,clk,en,input [Buffer_size-1:0] bufferInput,output [Buffer_size-1:0] bufferOut);
 	logic [Buffer_size-1:0] state;
 	
-	always @(negedge clk or posedge rst)begin
+	always @(negedge clk)begin
 		if(rst) state[Buffer_size-1:0]=0;
 		else begin
 			if(en)state=bufferInput;

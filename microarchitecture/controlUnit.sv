d6e74bdb1b862a69e731c32db4e29a173b957bf2
Luis Andrey Zuniga <luisandreyzuniga@gmail.com>
2023-04-28 22:32:29 -0600

    Adding control unit
    May change in the future with conditions not foreseen

---
+++ microarchitecture/controlUnit.sv
@@ -0,0 +1,14 @@
+module controlUnit(input [1:0] opType, input [3:0] opCode, output immSrc,branchFlag,memWrite,memToReg, output [3:0] aluControl);
+
+ //immSrc : es inmediato en alu
+ //brachFlag: es branch
+ //aluControl: operacion en alu
+ //memWrite: operacion escribe en memoria
+ //memToReg: de memoria a registro
+ 
+ 
+	immSrcControl myImmSrc(.opType(opType),.immSrc(immSrc));
+	branchFlagControl myBranchFlag (.opType(opType),.branchFlag(branchFlag));
+	aluControl myAluControl(.opType(opType),.opCode(opCode),.aluControl(aluControl));
+	memWriteControl myMemWriteControl(.opType(opType),.opCode(opCode),.memWrite(memWrite));
+endmodule 

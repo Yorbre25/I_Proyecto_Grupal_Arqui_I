ed95e8b22c287ecce702a90d0dcac398bb57c2a5
Yorbre25 <yraulbr25@gmail.com>
2023-05-13 14:00:32 -0600

    Add: MEM, WB to Proc

--- microarchitecture/writeBack.sv
+++ microarchitecture/writeBack.sv
@@ -1,4 +1,4 @@
-module writeBack #(parameter N=24)(input [N-1:0] readDataW, aluOutW, input memToReg, output logic [N-1:0] ResultW);
+module writeBack #(parameter N=24)(input [N-1:0] readDataW, aluOutW, input memToReg, output logic [N-1:0] resultW);
 	
 	mux21 #(.N(N)) mux1(
 	.a(readDataW), 

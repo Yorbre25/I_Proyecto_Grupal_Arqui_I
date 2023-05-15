d5b92e0b6f0999c67fc8afd997e39920b9b14ca7
Brian <briwag01@gmail.com>
2023-05-15 00:54:04 -0600

    passing image to greyscale and changing memory to be 8bits

--- Software/IMGMIF.py
+++ Software/IMGMIF.py
@@ -8,18 +8,17 @@ header = """
 DEPTH =  """
 
 header_2 = """;
-WIDTH = 6;
+WIDTH = 2;
 ADDRESS_RADIX = HEX;
 DATA_RADIX = HEX;
 CONTENT
 BEGIN\n"""
 
 if (len(sys.argv) < 2):
-    input_filename = 'Software/barbara300.png'
+    input_filename = 'Software/barbaraGrey300.png'
     output_filename = 'Software/barbara300.mif'
 
     im = Image.open(input_filename)
-
     f = open(output_filename, 'w');
 
     print("> Image size: ")
@@ -31,43 +30,29 @@ if (len(sys.argv) < 2):
     print("> Writing to file: "+ output_filename)
     lines,index = SinMif()
     f.write(header)
-    f.write(str(24))
+    f.write(str(8))
     f.write(header_2)
 
 
     for x in range(0, w):
         for y in range(0, h):
-            r = im.getpixel((x,y))[0] & 255
-            g = im.getpixel((x,y))[1] & 255
-            b = im.getpixel((x,y))[2] & 255
-
-            total = r<< 16| g << 8 | b ;
-
-
+            total = im.getpixel((x,y))& 255
             hexa = hex(total)
-
             if (total == 0):
-                hexa = "0x0000"
-
+                hexa = "0x00"
+            while len(hexa[2:]) < 2:
+                hexa = f'0x0{hexa[2:]}'
             lines.append(hex(index)[2:] + ":\t"+hexa[2:]+";\n")
-
             index += 1
     for x in range(0, w):
         for y in range(0, h):
-            r = im.getpixel((x,y))[0] & 255
-            g = im.getpixel((x,y))[1] & 255
-            b = im.getpixel((x,y))[2] & 255
-
-            total = r<< 16| g << 8 | b ;
-
-
+            total = im.getpixel((x,y))& 255
             hexa = hex(total)
-
             if (total == 0):
-                hexa = "0x0000"
-
+                hexa = "0x00"
+            while len(hexa[2:]) < 2:
+                hexa = f'0x0{hexa[2:]}'
             lines.append(hex(index)[2:] + ":\t"+hexa[2:]+";\n")
-
             index += 1
     f.writelines(lines)
     f.write("END;")
